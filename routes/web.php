caa988c2f85e65bb67566d938688371e78be081f
Maxi-M <maxim.moskovskiy@mail.ru>
2020-03-24 14:29:22 +0300

    Pages added

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', static function () {
+    return view('lesson1.first');
+});
+Route::get('/second', static function () {
+    return view('lesson1.second');
+});
+Route::get('/third', static function () {
+    return view('lesson1.third');
 });
