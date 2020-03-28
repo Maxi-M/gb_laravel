062b77c53746cdd899c2e55618a5a5c800337309
Maxi-M <maxim.moskovskiy@mail.ru>
2020-03-28 12:02:22 +0300

    Commit 1

---
+++ app/News.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+class News
+{
+    private static $news = [
+        1 => [
+            'id' => 1,
+            'title' => 'Новость 1',
+            'text' => 'А у нас новость 1 и она очень хорошая!'
+        ],
+        2 => [
+            'id' => 2,
+            'title' => 'Новость 2',
+            'text' => 'А тут плохие новости((('
+        ],
+    ];
+
+    public static function getNews()
+    {
+        return static::$news;
+    }
+
+    public static function getNewsId(int $id): ?array
+    {
+        return static::getNews()[$id] ?? null;
+    }
+}
