08c529810bc2a59a74df7fdd91edc13e9d8f2a48
Maxi-M <maxim.moskovskiy@mail.ru>
2020-04-07 15:41:10 +0300

    some changes out of homework scope

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-30 13:09:35.
+    * Generated on 2020-03-31 10:43:58.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
